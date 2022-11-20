7330865af9f1d5fa2b1ece8dc140b53ecb5f0bcd
Jong Hun Kim <corner.jhkim@gmail.com>
2022-11-20 17:41:03 +0900

    Added contents been a while

--- main.dart
+++ main.dart
@@ -4,7 +4,36 @@ void main() {
   int x = 12;
   example.printRslt(x);
 
-  int y = 11;
-  int z = 12;
+  int y = 14;
+  int z = 21;
   example.calcNum(y, z);
+
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+
+  var list = [1, 2, 4, 8];
+
+  for (int p = 0; p < list.length; p++) {
+    print(list[p]);
+    if (list[p] == 8) {
+      print("complete");
+    }
+  }
+
+  int val = 0;
+
+  while (val < 11) {
+    if (val % 2 == 0) {
+      print(val);
+    }
+    val++;
+    if (val == 11) {
+      print("the end");
+    }
+  }
+
+  for (int cnt = 0; cnt < 6; cnt++) {
+    print("*" * cnt);
+  }
 }
