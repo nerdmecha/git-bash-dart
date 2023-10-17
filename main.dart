363993c553d5f21bd817eb5c346e9a8ad8afa03f
Jonghun Kim <corner.jhkim@gmail.com>
2023-10-17 14:38:17 +0900

    Added Comments

--- main.dart
+++ main.dart
@@ -2,15 +2,15 @@ import 'lib/example.dart' as example;
 
 void main() {
   int x = 12;
-  example.printRslt(x);
+  example.printRslt(x); // Number A is x
 
   int y = 14;
   int z = 21;
-  example.calcNum(y, z);
+  example.calcNum(y, z); // 둣 숫자의 합은 25 이상입니다. or 미만입니다.
 
   for (int i = 0; i < 5; i++) {
     print(i);
-  }
+  } // 0, 1, 2, 3, 4
 
   var list = [1, 2, 4, 8];
 
@@ -19,7 +19,7 @@ void main() {
     if (list[p] == 8) {
       print("complete");
     }
-  }
+  } // 1, 2, 4, 8
 
   int val = 0;
 
@@ -31,9 +31,9 @@ void main() {
     if (val == 11) {
       print("the end");
     }
-  }
+  } // 0, 2, 4, 6, 8, 10
 
   for (int cnt = 0; cnt < 6; cnt++) {
     print("*" * cnt);
-  }
+  } // *, **, ***, ****, *****
 }
